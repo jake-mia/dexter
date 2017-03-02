87fee00725f9360765e2b73267195235c9a1c405
Jake Porter <jake.porter@yahoo.com>
2017-03-02 15:50:15 -0500

    added link for + and nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :challenges do
   resources :steps
-  resources :challenges
+end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
