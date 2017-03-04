02207d94475f8515d02948f32ccdf3d72db397bd
Orianna Casanas <orianna0826@gmail.com>
2017-03-04 14:38:50 -0500

    fixed challenge link positioning on index page

--- app/controllers/userchallenges_controller.rb
+++ app/controllers/userchallenges_controller.rb
@@ -2,7 +2,7 @@ class UserchallengesController < ApplicationController
 #session[:user_id] = user.id
 
   def enroll
-    @user_id = User.find(session[:user_id])
+    @user_id = current_user
     @challenges = Challenge.all
     @userchallenge = UserChallenge.create
   end
