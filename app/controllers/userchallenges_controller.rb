e2bfa35441eaee99e1fad5954fb5702e0a30b01e
Orianna Casanas <orianna0826@gmail.com>
2017-03-06 15:36:15 -0500

    chooses between food and fitness

--- app/controllers/userchallenges_controller.rb
+++ app/controllers/userchallenges_controller.rb
@@ -11,10 +11,12 @@ class UserchallengesController < ApplicationController
   def create
     @userchallenge = current_user.user_challenges.create!(userchallenge_params)
     #create the steps
-    step_args  = [
-      [
-        "Food",
-        [
+   challenge = Challenge.find(params[:user_challenge][:challenge_id])
+    message = "challenge is nil!!!"
+    raise new RuntimeError.new message if challenge.nil?
+    challenge_name = challenge.name
+    step_args  = {
+      "Food" => [
           {
             name: "foo",
             description: "foo info",
@@ -33,21 +35,36 @@ class UserchallengesController < ApplicationController
             completed: false,
             complete_by: Time.now.utc
           },
-        ]
-      ], # end of first set of steps args
-    ]
-
+        ], # end of first set of steps args
+      "Fitness" => [
+        {
+          name: "foo Fitness",
+          description: "foo Fitness info",
+          completed: false,
+          complete_by: Time.now.utc
+        },
+        {
+          name: "bar Fitness",
+          description: "bar Fitness info",
+          completed: false,
+          complete_by: Time.now.utc
+        },
+        {
+          name: "waz Fitness",
+          description: "waz Fitness info",
+          completed: false,
+          complete_by: Time.now.utc
+        },
+      ],
+    }.fetch(challenge_name)
     user = User.find(session[:user_id])
-    step_args.each do |challenge_name, step_args|
-      challenge = Challenge.find_by(name: challenge_name)
+    step_args.each do |args|
+      # challenge = Challenge.find_by(name: challenge_name)
       user_challenge = UserChallenge.find_by(challenge: challenge, user: user)
       message = "user challenge is nil!!!"
       raise RuntimeError.new message if user_challenge.nil?
-      step_args.each do |args|
-        # is nil?
-        args[:challenge] = user_challenge.challenge
-        Step.create(args)
-      end
+      args[:challenge] = user_challenge.challenge
+      Step.create(args)
     end
     redirect_to root_path, notice: "Challenge Accepted!"
   end
